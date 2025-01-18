f4a58e4ee9cc9fa53952a320d317a3f4233a29a0
sohamdasx <sohamdas.2702@gmail.com>
2025-01-19 03:49:06 +0530

    ugh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Survey from "./components/Survey";
 import Recommendations from "./components/Recommendations";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/recommendations" element={<Recommendations />} />
       </Routes>
